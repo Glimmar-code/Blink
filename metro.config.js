8d66773dbf32b1a0ac79172c8f9f519ece6e6121
Glimmar <golowosile@gmail.com>
2026-07-29 20:38:26 +0100

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,9 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.platforms = ['web', 'ios', 'android'];
 config.resolver.unstable_enablePackageExports = false;
 
-module.exports = config;
+module.exports = withNativeWind(config, { input: './global.css' });
