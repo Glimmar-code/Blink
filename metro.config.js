2f05f1c55723cfd7bcd0f071138bb48c53be6e4d
Glimmar <golowosile@gmail.com>
2026-08-06 02:38:59 +0100

    Delete metro.config.js

--- metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
-
-const config = getDefaultConfig(__dirname);
-
-config.resolver.platforms = ['web', 'ios', 'android'];
-config.resolver.unstable_enablePackageExports = false;
-
-module.exports = withNativeWind(config, { input: './global.css' });
